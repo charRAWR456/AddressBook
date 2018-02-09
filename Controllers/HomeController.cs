3c057cf051ab94251f13bc989f418bdb769f474b
Jamie Pittak <charrawr456@gmail.com>
2018-02-09 10:59:34 -0800

    add routing between pages, add contact functionality not working due to list bug

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,10 +6,33 @@ namespace AddressBook.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Home()
     {
       return View();
     }
+    [HttpGet("/addcontact")]
+    public ActionResult AddContactForm()
+    {
+      return View();
+    }
+    [HttpPost("/addcontact")]
+    public ActionResult AddContact()
+    {
+      string contactName = Request.Form[("contact-name")];
+      string contactAddress = Request.Form[("contact-address")];
+      Contact newContact = newContact(contactName, contactAddress);
+      newContact.Save();
+
+      List<Contact> allContacts = Contact.GetAll();
+      return View("AddressList", allContacts);
+    }
+
+    // [httpGet("/{id}")]
+    // public ActionResult ContactDetails(int id)
+    // {
+    //   Contact found contact = Contact.Find(id);
+    //   return View("FoundContact")
+    // }
   }
 }
--- Models/AddressBook.cs
+++ Models/AddressBook.cs
@@ -8,5 +8,52 @@ namespace AddressBook.Models
     private string _name;
     private string _address;
     private int _id;
+
+    private static List<ContactList> _contactList = new List<ContactList>{};
+
+    public void Contact(string contactName, string contactAddress)
+    {
+      _name = contactName;
+      _address = contactAddress;
+    }
+    public string GetContactName()
+    {
+      return _name;
+    }
+    public void SetContactName(string newContactName)
+    {
+      _name = newContactName;
+    }
+    public string GetContactAddress()
+    {
+      return _address;
+    }
+    public void GetContactAddress(string newContactAddress)
+    {
+      _address = newContactAddress;
+    }
+    public int GetID()
+    {
+      return _id;
+    }
+    public void Save()
+    {
+      _contactList.Add(this);
+      _id = _contactList.Count;
+    }
+    public static List<ContactList> GetContactList()
+    {
+      return _contactList;
+    }
+    public static void ClearAll()
+    {
+      _contactList.Clear();
+    }
+
+    public static Contact Find(int id)
+    {
+      return _contactList[id -1];
+
+    }
   }
 }
